edad3e98ababdc7657ff62791af662e48d06e9e3
walterh <waltermwfs@gmail.com>
2022-06-12 11:47:54 -0400

    Initial commit of JSON parsing of a result

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use serde::Deserialize;
+use std::error::Error;
+
+#[derive(Deserialize)]
+struct Quote {
+    ask: f32,
+}
+
+#[derive(Deserialize)]
+struct OptionResult {
+    quote: Quote,
+}
+
+#[derive(Deserialize)]
+struct OptionChain {
+    result: Vec<OptionResult>,
+}
+// don't know what error is returned yet
+#[derive(Deserialize)]
+struct ResponseError {}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Response {
+    option_chain: OptionChain,
+    error: ResponseError,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let resp = reqwest::get("https://query2.finance.yahoo.com/v7/finance/options/AAPL")
+        .await?
+        .json::<Response>()
+        .await?;
+
+    println!("{}", resp.option_chain.result[0].quote.ask);
+    Ok(())
+}
